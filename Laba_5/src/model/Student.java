5a23e9ac0d50510dd82229d9dd87d922793f4d12
renya_wa <151263469+corvula@users.noreply.github.com>
2025-10-30 18:17:53 +0200

    Create Student.java

---
+++ Laba_5/src/model/Student.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public double getGrade() { return grade; }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareToIgnoreCase(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (age: %d, grade: %.2f)", name, age, grade);
+    }
+}
