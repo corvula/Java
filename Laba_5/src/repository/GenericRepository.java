63f74a94524cddbe787cd3aab7b8a480074d687c
renya_wa <151263469+corvula@users.noreply.github.com>
2025-11-10 17:16:10 +0200

    Create GenericRepository.java

---
+++ Laba_5/src/repository/GenericRepository.java
@@ -0,0 +1,29 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import util.LoggerUtil;
+
+public class GenericRepository<T extends Comparable<T>> {
+    protected List<T> data = new ArrayList<>();
+
+    public void add(T item) {
+        data.add(item);
+        LoggerUtil.log("Added: " + item);
+    }
+
+    public List<T> getAll() {
+        return new ArrayList<>(data);
+    }
+
+    public void sortByIdentity(String order) {
+        if (order.equalsIgnoreCase("asc")) {
+            Collections.sort(data);
+        } else if (order.equalsIgnoreCase("desc")) {
+            data.sort(Collections.reverseOrder());
+        }
+        LoggerUtil.log("Sorted by identity (" + order + ")");
+    }
+}
