5107eb3bc6087ecf32dbcc9ef1e4f1183f11807d
renya_wa <151263469+corvula@users.noreply.github.com>
2025-11-10 17:32:07 +0200

    Update Student.java

--- Laba_5/src/model/Student.java
+++ Laba_5/src/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Student implements Comparable<Student> {
     private String name;
     private int age;
@@ -11,17 +13,36 @@ public class Student implements Comparable<Student> {
         this.grade = grade;
     }
 
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public double getGrade() { return grade; }
-
     @Override
     public int compareTo(Student other) {
         return this.name.compareToIgnoreCase(other.name);
     }
 
+    public static final java.util.Comparator<Student> BY_AGE =
+            (s1, s2) -> Integer.compare(s1.age, s2.age);
+
+    public static final java.util.Comparator<Student> BY_GRADE =
+            java.util.Comparator.comparingDouble(Student::getGrade);
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public double getGrade() { return grade; }
+
     @Override
     public String toString() {
-        return String.format("%s (age: %d, grade: %.2f)", name, age, grade);
+        return name + " (" + age + " y.o., " + grade + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student s = (Student) o;
+        return age == s.age && Double.compare(s.grade, grade) == 0 && Objects.equals(name, s.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, grade);
     }
 }
